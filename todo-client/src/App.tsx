ae278a158f0fedacd6f013dda77c7d3960b8694f
develop <develop@developnoMacBook-Air.local>
2025-08-30 23:05:34 +0900

    TODO LIST完成

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,5 +1,7 @@
-import { useQuery } from "@apollo/client/react";
+import { useMutation, useQuery } from "@apollo/client/react";
 import { gql } from "@apollo/client"
+import { useState } from "react";
+import { Variable } from "lucide-react";
 
 const GET_TODOS = gql`
   query{
@@ -9,7 +11,27 @@ const GET_TODOS = gql`
       completed
     }
   }
-`
+`;
+
+const ADD_TODO = gql`
+  mutation addTodo($title: String!) {
+      addTodo(title: $title) {
+        id,
+        title,
+        completed
+      }
+    }
+`;
+
+const UPDATE_TODO = gql`
+  mutation updateTodo($id: ID!, $completed: Boolean!) {
+    updateTodo(id: $id, completed: $completed) {
+      id,
+      title,
+      completed
+    }
+  }
+`;
 
 type Todo = {
   id: string;
@@ -18,31 +40,43 @@ type Todo = {
 }
 
 function App() {
-  // const todos = [
-  //   { id : "1", title: "task_a", completed: false },
-  //   { id : "2", title: "task_b", completed: false }
-  // ] as Todo[];
-
   const { loading, data } = useQuery(GET_TODOS, {
     fetchPolicy :"network-only",
   });
 
   const todos = data ? data.getTodos : []; 
+  const [addTodo] = useMutation(ADD_TODO);
+  const [updateTodo] = useMutation(UPDATE_TODO)
+  const [title, setTitle] = useState("");
+  const handleAddTodo = async () => {
+    await addTodo({
+      variables: { title },
+      refetchQueries: [{ query: GET_TODOS }]
+    })
+    setTitle("")
+  }
+
+  const handleUpdateTodo = async (id: string, completed: boolean) => {
+    await updateTodo({
+      variables: { id, completed: !completed  },
+      refetchQueries: [{ query: GET_TODOS }]
+    })
+  }
 
   if (loading) return <p>Loading....</p>
 
   return (
     <div>
       <h1>TODO List</h1>
-      <input type="text" placeholder="Please Add Todo"/>
-      <button>add</button>
+      <input type="text" placeholder="Please Add Todo" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <button onClick={handleAddTodo}>add</button>
       <ul>
         {todos.map((todo: Todo) => (
           <li key={todo.id} style={{
             textDecoration: todo.completed ? "line-through" : "none"
           }}
           >
-            <input type="checkbox" checked={todo.completed} />
+            <input type="checkbox" checked={todo.completed} onChange={() => handleUpdateTodo(todo.id, todo.completed)}/>
             {todo.title}
           </li>
         ))}
